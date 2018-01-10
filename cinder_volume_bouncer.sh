ac2de649f2380125433f64b09984e77c084a267c
seancarlisle <carlisle.sean@gmail.com>
2018-01-10 13:02:46 -0600

    Added PATH and cleaned up

--- cinder_volume_bouncer.sh
+++ cinder_volume_bouncer.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 # Source our credentials
 . /root/openrc
 
 # Check the status of the cinder-volume service
 echo "Checking for down services..."
+
 down_service_list=''
 down_service_list=$(cinder service-list | awk -F\| '/cinder-volume/ && /down/ {print $3":"$2}' | tr -d ' ')
 
@@ -13,7 +16,7 @@ if [ "$down_service_list" != '' ];then
    export IFS=$'\n'
    for i in $(echo $down_service_list); do
       echo "Found down cinder service $(echo $i | cut -d\: -f2) on host $(echo $i | cut -d\: -f1 | cut -d\@ -f1). Restarting..."
-      ssh $(echo $i | cut -d\: -f1 | sed 's/-cinder-volumes-container/_cinder_volumes_container/') "service $(echo $i | cut -d\: -f2) restart"
+      ssh $(echo $i | cut -d\: -f1 | sed 's/-cinder-volumes-container/_cinder_volumes_container/' | cut -d\@ -f1) "service $(echo $i | cut -d\: -f2) restart"
       echo "Done..."
    done
 fi
